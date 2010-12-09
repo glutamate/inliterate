cb815979d1d13703e6c36e083c34970968fd2d89
Tom Nielsen <tomn@tomn-eee.(none)>
2010-12-09 21:40:53 +0000

    begin gnuplot in ask

--- Ask.hs
+++ Ask.hs
@@ -18,7 +18,25 @@ instance Ask Double where
 instance Ask [Char] where 
    askInlit _ = return 
 
- 
+data PlotDims = PlotDims Double Double Int GnuplotBox
+
+instance Ask GnuplotBox where
+   askInlit om (GnuplotBox x)= askInLit om (PlotDims 3.5 5.0 16)
+       
+instance Ask PlotDims where
+   askInlit om (PlotDims w h fs (GnuplotBox x))= do
+      nm<- (("l":) . show . hashUnique) `fmap` newUnique
+      cmd <- multiPlot unitRect x
+      let start = "set datafile missing \"NaN\"\n"
+      let term = "set terminal postscript eps enhanced color \"Helvetica\" "++show fs++" size "++ show w++","++show h++"\n"-- crop\n"
+      let plot1 = "set output '"++nm++".eps'\n"++
+                               (showMultiPlot cmd)
+      let cmds = start++term ++plot1
+      execGP cmds 
+      system $ "epstopdf "++nm++".eps"
+      cleanupCmds $ map snd cmd
+
+
 ask :: Ask a => OutputMode -> [String] -> a -> IO ()
 ask om ss x = do
   mapM_ (putStrLn . ("?> "++)) ss
