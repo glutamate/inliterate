79a0736161b537e20a18a6a3c3aec8b37072340c
Tom Nielsen <tomn@tomn-desktop.(none)>
2010-12-10 11:39:38 +0000

    minor

--- Ask.hs
+++ Ask.hs
@@ -31,7 +31,9 @@ askLatexPic (PlotDims w h fs (GnuplotBox x))= do
       nm<- getNm
       cmd <- multiPlot unitRect x
       let start = "set datafile missing \"NaN\"\n"
-      let term = "set terminal pdfcairo color font \"Helvetica,"++show fs++"\" size "++ show w++" mm,"++show h++" mm\n"-- crop\n"
+      let term = "set terminal pdfcairo color font \"Helvetica,"
+                 ++show fs++"\" size "++ show w++" mm,"
+                 ++show h++" mm\n"-- crop\n"
       let plot1 = "set output '"++nm++".pdf'\n"++
                                (showMultiPlot cmd)
       let cmds = start++term ++plot1
@@ -43,7 +45,9 @@ askHtmlPic (PlotDims w h fs (GnuplotBox x))= do
       nm<- getNm
       cmd <- multiPlot unitRect x
       let start = "set datafile missing \"NaN\"\n"
-      let term = "set terminal png font \"Helvetica,"++show fs++"\" size "++ show w++","++show h++"\n"-- crop\n"
+      let term = "set terminal png font \"Helvetica,"
+                 ++show fs++"\" size "++ show w++","
+                 ++show h++"\n"-- crop\n"
       let plot1 = "set output '"++nm++".png'\n"++
                                (showMultiPlot cmd)
       let cmds = start++term ++plot1
